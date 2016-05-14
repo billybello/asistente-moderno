5011035306a272441a73b294ea4573a49666f282
Samuel Reinoso <hackeadorazo@gmail.com>
2016-05-14 19:49:52 -0400

    upload files

---
+++ AsistenteModerno/Connection.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsistenteModerno
+{
+    class Connection
+    {
+        public static SqlConnection getconnection()
+        {
+            SqlConnection con = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\asistentemoderno.mdf;Integrated Security=True");
+            return con;
+        }
+    }
+}
---
+++ AsistenteModerno/Login.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace AsistenteModerno
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection cnn = Connection.getconnection();
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                cnn.Open();
+                cnn.Close();
+                MessageBox.Show("Conectado a la base de datos");
+            } catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+    }
+}
