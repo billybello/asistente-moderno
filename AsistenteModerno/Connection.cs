3697f49c45cfc5d2bba69918cc0ae0cced7ad0cb
Samuel Reinoso <hackeadorazo@gmail.com>
2016-05-14 20:59:56 -0400

    agregado la clase connection

--- AsistenteModerno/Connection.cs
+++ AsistenteModerno/Connection.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
@@ -13,6 +14,30 @@ namespace AsistenteModerno
         {
             SqlConnection con = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\asistentemoderno.mdf;Integrated Security=True");
             return con;
+        }
+
+        public static DataTable getDataTable(string query)
+        {
+            SqlConnection cnn = Connection.getconnection();
+            cnn.Open();
+            SqlDataAdapter da = new SqlDataAdapter(query, cnn);
+            SqlCommandBuilder commandBuilder = new SqlCommandBuilder(da);
+            DataTable table = new DataTable();
+            da.Fill(table);
+            cnn.Close();
+            return table;
+        }
+
+        public static SqlDataReader getDataReader(string query)
+        {
+            SqlConnection cnn = Connection.getconnection();
+            cnn.Open();
+            SqlDataReader dr = null;
+            SqlCommand consulta = new SqlCommand(query, cnn);
+            consulta.CommandType = CommandType.Text;
+            dr = consulta.ExecuteReader();
+            cnn.Close();
+            return dr;
         }
     }
 }
